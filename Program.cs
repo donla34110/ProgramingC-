c77a2f2898c789690376cca3cb9411e8792e9766
patjamin <uhfdyuggzz@gmail.com>
2024-09-11 11:04:36 +0700

    addC#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int num1 = int.Parse(Console.ReadLine()?? "0");
+        Console.Write("Enter second number: ");
+        int num2 = int.Parse(Console.ReadLine()?? "0");
+        int sum = num1 + num2;
+        Console.WriteLine("Sum: " + sum);
+    }
+}
